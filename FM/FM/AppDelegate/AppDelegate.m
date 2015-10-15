13a83e1779d2f47f2bc6dc2678830a2b04b3c66a
lanou3g <lanou3g@lanou3gdeMac-mini-10.local>
2015-10-15 15:42:38 +0800

    [update] 添加了启动图

--- FM/FM/AppDelegate/AppDelegate.m
+++ FM/FM/AppDelegate/AppDelegate.m
@@ -11,7 +11,7 @@
 #import "RadioController.h"
 #import "MineController.h"
 
-@interface AppDelegate ()
+@interface AppDelegate ()<UIScrollViewDelegate>
 
 @end
 
@@ -19,6 +19,11 @@
 
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
+    
+
+    
+
+
     
     [AVOSCloud setApplicationId:@"gygk6k80xeVgrlVM2PkeEzsW"
                       clientKey:@"PupC2Jsgb9wzzaDOuyW8hExC"];
@@ -57,10 +62,100 @@
     
     self.window.rootViewController = tabBarVC;
     
+    
     self.window.backgroundColor = [UIColor whiteColor];
     [self.window makeKeyAndVisible];
+    
+    
+    //启动图
+    NSUserDefaults *userDefaults = [NSUserDefaults standardUserDefaults];
+    
+    //判断滑动图是否出现过,第一次调用时"isScrollViewAppear"这个key对应的值是nil，会进入if中
+    if (![@"YES" isEqualToString:[userDefaults objectForKey:@"isScrollViewAppear"]]) {
+        //显示滑动图
+        [self showScrollView];
+    }
+    
     return YES;
 }
+
+
+-(void) showScrollView{
+    
+    UIScrollView *scrollView = [[UIScrollView alloc] initWithFrame:kScreenRect];
+    
+    //设置UIScrollView 的显示内容的尺寸，有n张图要显示，就设置 屏幕宽度*n ，这里假设要显示4张图
+    scrollView.contentSize = CGSizeMake([UIScreen mainScreen].bounds.size.width * 4, [UIScreen mainScreen].bounds.size.height);
+    
+    scrollView.tag = 100;
+    
+    //设置翻页效果，不允许反弹，不显示水平滑动条，设置代理为自己
+    scrollView.pagingEnabled = YES;
+    scrollView.bounces = NO;
+    scrollView.showsHorizontalScrollIndicator = NO;
+    scrollView.delegate = self;
+    
+    //在UIScrollView 上加入 UIImageView
+    for (int i = 0 ; i < 4; i ++) {
+        
+        UIImageView *imageView = [[UIImageView alloc] initWithFrame:CGRectMake(kScreenRect.size.width * i , 0, kScreenRect.size.width, [UIScreen mainScreen].bounds.size.height)];
+        
+        //将要加载的图片放入imageView 中
+        UIImage *image = [UIImage imageNamed:[NSString stringWithFormat:@"%d",i+1]];
+        imageView.image = image;
+        
+        [scrollView addSubview:imageView];
+    }
+    
+    //初始化 UIPageControl 和 _scrollView 显示在 同一个页面中
+    UIPageControl *pageConteol = [[UIPageControl alloc] initWithFrame:CGRectMake(0, 0, 50, 40)];
+    pageConteol.center = CGPointMake(kScreenWidth/2, kScreenHeight/1.1);
+    pageConteol.numberOfPages = 4;//设置pageConteol 的page 和 _scrollView 上的图片一样多
+    pageConteol.tag = 200;
+    
+    [self.window addSubview:scrollView];
+    [self.window addSubview: pageConteol];
+}
+
+
+- (void)scrollViewDidEndDecelerating:(UIScrollView *)scrollView{
+    
+    // 记录scrollView 的当前位置，因为已经设置了分页效果，所以：位置/屏幕大小 = 第几页
+    int current = scrollView.contentOffset.x/kScreenRect.size.width;
+    
+    //根据scrollView 的位置对page 的当前页赋值
+    UIPageControl *page = (UIPageControl *)[self.window viewWithTag:200];
+    page.currentPage = current;
+    
+    //当显示到最后一页时，让滑动图消失
+    if (page.currentPage == 3) {
+        
+        //调用方法，使滑动图消失
+        [self scrollViewDisappear];
+    }
+}
+-(void)scrollViewDisappear{
+    
+    //拿到 window 中的 UIScrollView 和 UIPageControl
+    UIScrollView *scrollView = (UIScrollView *)[self.window viewWithTag:100];
+    UIPageControl *page = (UIPageControl *)[self.window viewWithTag:200];
+    
+    //设置滑动图消失的动画效果图
+    [UIView animateWithDuration:3.0f animations:^{
+        
+        scrollView.center = CGPointMake(self.window.frame.size.width/2, 1.5 * self.window.frame.size.height);
+        
+    } completion:^(BOOL finished) {
+        
+        [scrollView removeFromSuperview];
+        [page removeFromSuperview];
+    }];
+    
+    //将滑动图启动过的信息保存到 NSUserDefaults 中，使得第二次不运行滑动图
+    NSUserDefaults *userDefaults = [NSUserDefaults standardUserDefaults];
+    [userDefaults setObject:@"YES" forKey:@"isScrollViewAppear"];
+}
+
 
 - (void)applicationWillResignActive:(UIApplication *)application {
     // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
